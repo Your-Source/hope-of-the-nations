4b4fff0501ad547f0eb95e0a97e233e8d57e5c32
fabian <fabian@your-source.nl>
2014-07-29 14:14:13 +0200

    feature/1910-create-overview-page - Add functions to hotnSponsorChild and add interface functions

--- lib/hotnSponsorChild.php
+++ lib/hotnSponsorChild.php
@@ -4,5 +4,86 @@
  */
 
 class hotnSponsorChild implements hotnSponsorChildInterface {
+  private $child = array();
+
+  /**
+   * Construct function for get all child properties.
+   * @param array $child Array with all child properties.
+   */
+  function __construct($child) {
+    $this->child = $child;
+  }
+
+  /**
+   * Returns int with child id.
+   */
+  public function getChildId() {
+    return $this->child['ChildID'];
+  }
+
+  /**
+   * Returns string with child story.
+   */
+  public function getChildStory() {
+    return $this->child['Story'];
+  }
+
+  /**
+   * Returns string with child hobbies.
+   */
+  public function getChildHobbies() {
+    return $this->child['Hobbies'];
+  }
+
+  /**
+   * Returns string with child country.
+   */
+  public function getChildCountry() {
+    return $this->child['Country'];
+  }
+
+  /**
+   * Returns string with child birthday.
+   */
+  public function getChildBirthdate() {
+    return $this->child['Birthdate'];
+  }
+
+  /**
+   * Returns string with child age.
+   */
+  public function getChildAge() {
+    $birthdate = $this->child['Birthdate'];
+
+    // Create date from birthdate.
+    $date = new DateTime($birthdate);
+    $now = new DateTime();
+    // Get diff between now and birthdate.
+    $interval = $now->diff($date);
+    $age = $interval->y;
+
+    return $age;
+  }
+
+  /**
+   * Returns string with child gender.
+   */
+  public function getChildGender() {
+    return $this->child['Gender'];
+  }
+
+  /**
+   * Returns string with url to small child image.
+   */
+  public function getChildSmallImage() {
+    return $this->child['SmallImage'];
+  }
+
+  /**
+   * Returns string with url to large child image.
+   */
+  public function getChildLargeImage() {
+    return $this->child['LargeImage'];
+  }
 
 }
--- lib/hotnSponsorChildInterface.php
+++ lib/hotnSponsorChildInterface.php
@@ -5,4 +5,54 @@
 
 interface hotnSponsorChildInterface {
 
+  /**
+   * Construct function for get all child properties.
+   * @param array $child Array with all child properties.
+   */
+  function __construct($child);
+
+  /**
+   * Returns int with child id.
+   */
+  public function getChildId();
+
+  /**
+   * Returns string with child story.
+   */
+  public function getChildStory();
+
+  /**
+   * Returns string with child hobbies.
+   */
+  public function getChildHobbies();
+
+  /**
+   * Returns string with child country.
+   */
+  public function getChildCountry();
+
+  /**
+   * Returns string with child birthday.
+   */
+  public function getChildBirthdate();
+
+  /**
+   * Returns string with child age.
+   */
+  public function getChildAge();
+
+  /**
+   * Returns string with child gender.
+   */
+  public function getChildGender();
+
+  /**
+   * Returns string with url to small child image.
+   */
+  public function getChildSmallImage();
+
+  /**
+   * Returns string with url to large child image.
+   */
+  public function getChildLargeImage();
 }
