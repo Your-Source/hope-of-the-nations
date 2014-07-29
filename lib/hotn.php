86128f67ca4a9a27c3ca601704483d1679e407e3
fabian <fabian@your-source.nl>
2014-07-29 16:13:13 +0200

    feature/1910-create-overview-page - Add t function and add theme function for overview

--- lib/hotn.php
+++ lib/hotn.php
@@ -10,15 +10,29 @@ include_once __dir__ . '/hotnSponsorChild.php';
 class hotn {
 
   public function get_overview() {
-    $childs = hotnConnector::get_feed();
+    $childs = hotnConnector::get_feed('child', $_GET);
 
+    if (empty($childs)) {
+      return self::t('There are no children available.');
+    }
+    $child_output = array();
     foreach ($childs as $child) {
       $c = new hotnSponsorChild($child);
 
-      echo self::theme_overview_child($c);
+      $child_output[] = self::theme_overview_child($c);
     }
 
+    return self::theme_overview($child_output);
+  }
 
+  public function t($string) {
+    $translator_func = hotnConfig::$translator_func;
+
+    if (function_exists($translator_func)) {
+      return call_user_func($translator_func, array($string));
+    }
+
+    return $string;
   }
 
   /**
@@ -41,6 +55,41 @@ class hotn {
     $output .= '<span class="birthdate">' . $child->getChildBirthdate() . '</span>';
     $output .= '<br />';
     $output .= '<span class="more-info"><a href="#">More info</a></span>';
+    $output .= '<br />';
+
+    $output .= '</div>';
+
+    return $output;
+  }
+
+  /**
+   * Theme function for child items on overview page.
+   */
+  private function theme_overview(array $childs) {
+    $output = '<div>';
+
+    $output .= '<form method="get"> ';
+    $output .= '<select name="hotn-gender"> ';
+    $output .= '  <option value="">Selecteer</option> ';
+    $output .= '  <option value="Jongen">Jongen</option> ';
+    $output .= '  <option value="Meisje">Meisje</option> ';
+    $output .= '</select>';
+    $output .= '<select name="hotn-agegroup"> ';
+    $output .= '  <option value="">Selecteer</option> ';
+    $output .= '  <option value="0">below 3</option> ';
+    $output .= '  <option value="1">3 - 6</option> ';
+    $output .= '  <option value="2">7 - 9</option> ';
+    $output .= '  <option value="3">10 or above</option> ';
+    $output .= '</select>';
+    $output .= '<input type="submit" value="Submit">';
+    $output .= '</form>';
+
+    $output .= '<div class="child-list">';
+
+    foreach ($childs as $child) {
+      $output .= $child;
+    }
+
     $output .= '</div>';
 
     $output .= '</div>';
