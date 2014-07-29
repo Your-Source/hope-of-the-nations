ceaab923a3d20d07fff8d26de3897319ac4b90a0
fabian <fabian@your-source.nl>
2014-07-29 14:42:45 +0200

    Load all childs and create theme function for show on the screen

--- lib/hotn.php
+++ lib/hotn.php
@@ -10,6 +10,41 @@ include_once __dir__ . '/hotnSponsorChild.php';
 class hotn {
 
   public function get_overview() {
-    hotnConnector::get_feed();
+    $childs = hotnConnector::get_feed();
+
+    foreach ($childs as $child) {
+      $c = new hotnSponsorChild($child);
+
+      echo self::theme_overview_child($c);
+    }
+
+
+  }
+
+  /**
+   * Theme function for child items on overview page.
+   */
+  private function theme_overview_child(hotnSponsorChild $child) {
+    $output = '';
+
+    $output .= '<div class="item child-overview">';
+
+    $output .= '<div class="image">';
+    $output .= ' ';
+    $output .= '</div>';
+
+    $output .= '<div class="info">';
+    $output .= '<span class="name">' . $child->getChildName() . '</span>';
+    $output .= '<br />';
+    $output .= '<span class="country">' . $child->getChildCountry() . '</span>';
+    $output .= '<br />';
+    $output .= '<span class="birthdate">' . $child->getChildBirthdate() . '</span>';
+    $output .= '<br />';
+    $output .= '<span class="more-info"><a href="#">More info</a></span>';
+    $output .= '</div>';
+
+    $output .= '</div>';
+
+    return $output;
   }
 }
