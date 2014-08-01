4f62f615df6a142681f19786e7980fc19bcf3819
fabian <fabian@your-source.nl>
2014-08-01 13:23:30 +0200

    feature/1910-create-overview-page - Set debug standard to false

--- hotnConfig.php
+++ hotnConfig.php
@@ -26,7 +26,7 @@
  *  This function can be user defined.
  */
 class hotnConfig {
-  public static $debug = TRUE;
+  public static $debug = FALSE;
   public static $url = 'http://hotn.administratiekoppeling.nl';
   public static $apikey = 'b9a695eee0986c7774f80c885649c278';
   public static $childUrlData = array(
