76839e4b69beaced39b526c4207960d0918eff56
fabian <fabian@your-source.nl>
2014-08-13 16:26:19 +0200

    feature/1909-plugins - Add Joomla plugin

---
+++ plugins/Joomla/hotn/README.md
@@ -0,0 +1,14 @@
+# Hope of the Nations
+
+Module to show a list of childeren, child detail and sponsor a Child.
+
+To use this module you need to enable the module. You can place the module on the place you want en the page you want in the module administration.
+
+## Requirements
+* Library
+	* hotn
+
+## Library
+This module requires the library hotn. Place of this library is:
+
+* libraries/hotn
---
+++ plugins/Joomla/hotn/hotn.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Module file for Hope of the Nations.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$library_path = JPATH_BASE . DS . 'libraries' . DS . 'hotn';
+
+// If library is not exist set error message and return.
+if (!file_exists($library_path . DS . 'hotnConfig.php')) {
+  JError::raiseWarning(100, JText::_('The Hope of the Nations library is not present.'));
+
+  return;
+}
+
+// Include library files.
+require_once $library_path . DS . 'hotnConfig.php';
+require_once $library_path . DS . 'lib' . DS . 'hotn.php';
+
+// Include javascript an css file to document header.
+$document = JFactory::getDocument();
+$document->addScript(DS . 'libraries' . DS . 'hotn' . '/js/hotn.js');
+$document->addStyleSheet(DS . 'libraries' . DS . 'hotn' . '/css/hotn-style.css');
+
+print hotn::load();
